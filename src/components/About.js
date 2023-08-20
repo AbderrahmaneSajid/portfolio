cd83e7a2170614745a4c169f4fb4c83aae2a654e
AbderrahmaneSajid <sajidabderrahmane59@gmail.com>
2023-08-20 13:24:49 +0100

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,61 @@
 import React from 'react';
-
+//icons
+import { FaGithub, FaLinkedin, FaInstagram} from 'react-icons/fa'
+//image
+import Image from '../assets/moi.png'
+//intersection observer
+import {useInView} from 'react-intersection-observer'
+//motion
+import {motion} from 'framer-motion'
+//variants
+import {fadeIn} from '../variants'
 const About = () => {
-  return <div>About</div>;
+  const [ref, inView] = useInView({
+    threshold : 0.5,
+  })
+  return <section className='lg:pb-24 lg:pt-2 py-12 ' id='about' ref={ref}>
+    <div className='container mx-auto'>
+      <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center
+      lg:gap-x-20 lg:gap-y-0'>
+        {/* img */}
+        <motion.div variants={fadeIn('right',0.3)} initial="hidden"
+        whileInView={'show'} viewport={{once: false, amount:0.3}}>
+        <img src={Image} alt=''className=" max-w-max mx-auto h-[420px] lg:flex lg:h-[560px] " ></img>
+      </motion.div>
+        {/* text */}
+        <motion.div variants={fadeIn('left',0.5)} initial="hidden"
+        whileInView={'show'} viewport={{once: false, amount:0.3}}
+        className='flex-1'
+        >
+          <h2 className='h2 text-[30px] text-accent mt-8'>About me.</h2>
+          <h3 className='h3 text-[22px] mb-4'>I am currently a Software engineering student  at the National 
+            Institute of Posts and Telecommunications.</h3>
+            <p className='mb-1'>
+            I am passionate about coding and solving problems through code.
+           
+            </p>
+            <p className='mb-6'>
+            I spend most of my time designing and developing web and mobile applications
+            with good-looking UIs and a strong infrastructure.
+            </p>
+            <h3 className='h3 text-[20px] mb-4'>You can find me on :</h3>
+            {/* socials */}
+            <div 
+            className='flex text-[30px] gap-x-6 max-w-max mx-auto '>
+              <a href='https://github.com/AbderrahmaneSajid' target="_blank">
+                <FaGithub />
+              </a>
+              <a href='https://www.linkedin.com/in/abderrahmane-sajid-a6154b273/' target="_blank">
+                <FaLinkedin />
+              </a>
+              <a href='https://www.instagram.com/saaaaajiiiid/' target="_blank">
+                <FaInstagram />
+              </a>
+            </div>
+        </motion.div>
+      </div>
+    </div>
+    </section>;
 };
 
 export default About;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+//motion
+import {motion} from 'framer-motion'
+//variants
+import {fadeIn} from '../variants'
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_7sci0f4', 'template_u2qk80f', form.current, '3aEUv7-Pi3jZrg3qf')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+    }
+  return <div className='py-16 h-screen section ' id='contact'>
+    <div className='container mx-auto'>
+      <div className='flex flex-col lg:flex-row'>
+        {/* text */}
+        <motion.div variants={fadeIn('right',0.3)} initial="hidden"
+        whileInView={'show'} viewport={{once: false, amount:0.3}}
+        className='flex-1 flex justify-start items-center'>
+          <div>
+            <h4 className='text-xl uppercase text-accent font-medium 
+            mb-2 tracking-wide'>
+              Get in touch
+            </h4>
+            <h2 className='text-[45px] lg:text-[70px] leading-none mb-12'>Let's work <br /> together!</h2>
+          </div>
+        </motion.div>
+        {/* form */}
+        <motion.form variants={fadeIn('left',0.3)} initial="hidden"
+        whileInView={'show'} viewport={{once: false, amount:0.3}}
+        className='flex-1 border rounded-2xl flex flex-col gap-y-6
+        pb-24 p-6 items-start'
+        ref={form} onSubmit={sendEmail}>
+          <input className='bg-transparent border-b py-3 outline-none w-full
+          placeholder:text-white focus:border-accent transition-all
+          ' 
+          type='text'
+          placeholder='Your name'
+          name="user_name" />
+          <input className='bg-transparent border-b py-3 outline-none w-full
+          placeholder:text-white focus:border-accent transition-all
+          ' 
+          type='text'
+          placeholder='Your email'
+          name="user_email" />
+          <textarea className='bg-transparent border-b py-10 outline-none w-full
+          placeholder:text-white focus:border-accent transition-all
+          resize-none mb-12'
+          placeholder='Your message'
+          name="message"></textarea>
+          <button className='btn btn-lg'>Send message</button>
+        </motion.form>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Contact;
